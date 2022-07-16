9a4dc9f8834c10d54460dbdd62c1c36e62525274
平野 宇教 <82029019+TakayukiHirano117@users.noreply.github.com>
2022-07-17 07:30:41 +0900

    Update board_app_github.php

--- board_app_github.php
+++ board_app_github.php
@@ -9,7 +9,7 @@
 <body>
 <?php
 // データベースに接続
-$dsn = 'データベース名;host=localhost';
+$dsn = 'データベース名';
 $user = 'ユーザー名';
 $password = 'パスワード';
 $pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
